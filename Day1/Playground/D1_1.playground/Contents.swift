7b2fa4343e2417b7a5214283419c292403b8f2d6
Kiyong Kim <scgyong@gmail.com>
2023-07-10 13:58:16 +0900

    array append

--- Day1/Playground/D1_1.playground/Contents.swift
+++ Day1/Playground/D1_1.playground/Contents.swift
@@ -4,6 +4,16 @@ var a = 10
 a += 320
 a *= 230
 
+var value = Double(10)
+value
+
+let names = [ "hello", "world", "c" ]
+names.first
+names.last
+//names.append("d")
+//names.remove(at: 2)
+let n2 = names.sorted()
+
 var greeting = "Hello, playgrjiljefjlfewjound"
 print(greeting)
 

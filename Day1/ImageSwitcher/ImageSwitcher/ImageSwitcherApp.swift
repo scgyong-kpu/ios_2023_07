c731d6dac3c63259fa6db8ef8a3c26f1472b816a
Kiyong Kim <scgyong@gmail.com>
2023-07-10 15:45:48 +0900

    ImageSwitcher Skeleton

---
+++ Day1/ImageSwitcher/ImageSwitcher/ImageSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSwitcherApp.swift
+//  ImageSwitcher
+//
+//  Created by Kiyong Kim on 2023/07/10.
+//
+
+import SwiftUI
+
+@main
+struct ImageSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

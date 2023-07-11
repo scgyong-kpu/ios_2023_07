676cae234c9e96148b0fce70ce141f6e67e40633
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-07-11 15:15:04 +0900

    Slider 추가하고 값을 Text 에 표시

--- Day2/MoreControls/MoreControls/ContentView.swift
+++ Day2/MoreControls/MoreControls/ContentView.swift
@@ -8,17 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var rotDegrees = 0.0
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world! \(rotDegrees)")
             Image(systemName: "arrow.right.circle")
                 .resizable()
                 .aspectRatio(1.0, contentMode: .fit)
+                .frame(width: 200, height: 200)
                 .foregroundColor(.yellow)
                 .shadow(color: .red, radius: 10, x: 10, y: 10)
+            Slider(value: $rotDegrees)
+                .frame(width: 200)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(

f704a03a34d282634bdd36d75811fb6a92bcbf22
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-07-11 16:18:18 +0900

    Flags project skeleton

---
+++ Day2/Flags/Flags/FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsApp.swift
+//  Flags
+//
+//  Created by user on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

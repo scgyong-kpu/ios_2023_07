d0c310acb4a6320f4cb1c93a3568fe3192ea9c21
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-07-11 15:24:19 +0900

    ADDED TOGGLE

--- Day2/MoreControls/MoreControls/ContentView.swift
+++ Day2/MoreControls/MoreControls/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var rotDegrees = 0.0
+    @State var large = false
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -18,12 +19,16 @@ struct ContentView: View {
             Image(systemName: "arrow.right.circle")
                 .resizable()
                 .aspectRatio(1.0, contentMode: .fit)
-                .frame(width: 200, height: 200)
+                .frame(width: large ? 200 : 100, height: 200)
                 .foregroundColor(.yellow)
                 .rotationEffect(.degrees(rotDegrees * 360))
                 .shadow(color: .red, radius: 10, x: 10, y: 10)
             Slider(value: $rotDegrees)
                 .frame(width: 200)
+            Toggle(isOn: $large) {
+                Text("Shows Large Circle")
+            }
+            .padding()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(

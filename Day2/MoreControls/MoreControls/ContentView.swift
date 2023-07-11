8c470c33375ab12d3de8e02959c5f41a092000fb
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-07-11 15:17:54 +0900

    added rotation

--- Day2/MoreControls/MoreControls/ContentView.swift
+++ Day2/MoreControls/MoreControls/ContentView.swift
@@ -20,6 +20,7 @@ struct ContentView: View {
                 .aspectRatio(1.0, contentMode: .fit)
                 .frame(width: 200, height: 200)
                 .foregroundColor(.yellow)
+                .rotationEffect(.degrees(rotDegrees * 360))
                 .shadow(color: .red, radius: 10, x: 10, y: 10)
             Slider(value: $rotDegrees)
                 .frame(width: 200)

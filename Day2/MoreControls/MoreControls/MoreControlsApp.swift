5a12ff5d4d44bafcc5eefb115c748a89c5b3b4b3
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-07-11 14:56:16 +0900

    MoreControls skeleton

---
+++ Day2/MoreControls/MoreControls/MoreControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoreControlsApp.swift
+//  MoreControls
+//
+//  Created by user on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct MoreControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

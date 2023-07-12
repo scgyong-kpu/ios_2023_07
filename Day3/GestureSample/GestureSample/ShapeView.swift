2d8f49b5b0df1a474680d1f17bf7135844f89581
Kiyong Kim <scgyong@gmail.com>
2023-07-12 15:53:58 +0900

    여러 Drawing Shape 들 추가

--- Day3/GestureSample/GestureSample/ShapeView.swift
+++ Day3/GestureSample/GestureSample/ShapeView.swift
@@ -9,8 +9,45 @@ import SwiftUI
 
 struct ShapeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .navigationTitle("Shapes")
+        VStack {
+            Color.red
+                .frame(width: 200, height: 50)
+            Rectangle()
+                .stroke(lineWidth: 20)
+                .fill(
+                    LinearGradient(
+                        colors: [.red, .blue],
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing
+                    )
+                )
+                .frame(height: 50)
+                .cornerRadius(5)
+                .padding()
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 10)
+                .frame(height: 50)
+                .padding()
+            Capsule()
+                .stroke(lineWidth: 10)
+                .foregroundColor(.blue)
+                .frame(height: 100)
+                .padding(.horizontal)
+            Circle()
+                .frame(width: 100, height: 50)
+            Ellipse()
+                .frame(width: 100, height: 50)
+            Path { g in
+                g.move(to: .zero)
+                g.addLine(to: CGPoint(x: 100, y: 200))
+                g.addLine(to: CGPoint(x: 200, y: 100))
+                g.addLine(to: CGPoint(x: 150, y: 50))
+                g.closeSubpath()
+            }
+            .stroke(lineWidth: 5)
+            .fill(.blue)
+        }
+        .navigationTitle("Shapes")
     }
 }
 

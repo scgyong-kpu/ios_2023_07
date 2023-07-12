d17a7e0051defdb80ba8c5e3b01dc16f48953b32
Kiyong Kim <scgyong@gmail.com>
2023-07-12 12:02:42 +0900

    Nav->List->Link->Tap -> TapGestureView

--- Day3/GestureSample/GestureSample/MainView.swift
+++ Day3/GestureSample/GestureSample/MainView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                NavigationLink {
+                    TapGestureView()
+                } label: {
+                    Text("Tap")
+                }
+            }
+            .navigationTitle("Gestures Test")
         }
-        .padding()
     }
 }
 
---
+++ Day3/GestureSample/GestureSample/TapGestureView.swift
@@ -0,0 +1,20 @@
+//
+//  TapGestureView.swift
+//  GestureSample
+//
+//  Created by Kiyong Kim on 2023/07/12.
+//
+
+import SwiftUI
+
+struct TapGestureView: View {
+    var body: some View {
+        Text("Tap Me !")
+    }
+}
+
+struct TapGestureView_Previews: PreviewProvider {
+    static var previews: some View {
+        TapGestureView()
+    }
+}

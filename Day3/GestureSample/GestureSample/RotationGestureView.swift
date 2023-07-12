3cf2f493d5a1e4bd7d3146ab463007a86349e892
Kiyong Kim <scgyong@gmail.com>
2023-07-12 15:12:51 +0900

    RotationGesture, rotationEffect 구현

--- Day3/GestureSample/GestureSample/RotationGestureView.swift
+++ Day3/GestureSample/GestureSample/RotationGestureView.swift
@@ -7,9 +7,16 @@
 
 import SwiftUI
 
+//String(format: "%.1f", 10.2355)
+
 struct RotationGestureView: View {
+    @State var angle = Angle.zero
     var body: some View {
         VStack {
+            Text("Angle = \(String(format:"%.1f°", angle.degrees))")
+                .font(.headline)
+                .foregroundColor(.blue)
+            Spacer()
             VStack {
                 Image(systemName: "arrow.right.circle")
                     .font(.largeTitle)
@@ -21,6 +28,17 @@ struct RotationGestureView: View {
             }
             .foregroundColor(.purple)
             .background(Color.orange.opacity(0.3))
+            .rotationEffect(angle)
+            .gesture(
+                RotationGesture()
+                    .onChanged { value in
+                        angle = value
+                    }
+                    .onEnded { _ in
+                        angle = .zero
+                    }
+            )
+            Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.blue.opacity(0.2))

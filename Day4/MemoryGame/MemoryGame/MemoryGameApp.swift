fcc1b73774e88f86f2c42cf80fda79e337c0d139
Kiyong Kim <scgyong@gmail.com>
2023-07-13 10:40:50 +0900

    created MemoryGame project

---
+++ Day4/MemoryGame/MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Kiyong Kim on 2023/07/13.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

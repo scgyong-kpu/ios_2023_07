bd2724631a15fc7f4af53e4d20918ceaaf12d19e
Kiyong Kim <scgyong@gmail.com>
2023-07-13 14:58:23 +0900

    MenuView 를 NavigationView / List 로 표현.

--- Day4/MemoryGame/MemoryGame/MenuView.swift
+++ Day4/MemoryGame/MemoryGame/MenuView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct MenuView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                NavigationLink {
+                    GameView()
+                } label: {
+                    Text("Game")
+                }
+            }
+        }
     }
 }
 

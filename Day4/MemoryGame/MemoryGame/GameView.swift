33691a65236645ed8ce761a2934a268622201160
Kiyong Kim <scgyong@gmail.com>
2023-07-13 15:32:53 +0900

    GameView BG 꾸미기 (white - blue(0.5))

--- Day4/MemoryGame/MemoryGame/GameView.swift
+++ Day4/MemoryGame/MemoryGame/GameView.swift
@@ -64,6 +64,13 @@ struct GameView: View {
             }
             Spacer()
         }
+        .background(
+            LinearGradient(
+                colors: [ .white, .blue.opacity(0.5) ],
+                startPoint: .topLeading,
+                endPoint: .bottomLeading
+            )
+        )
         .alert(isPresented: $showsRetryAlert) {
             Alert(
                 title: Text("Restart"),

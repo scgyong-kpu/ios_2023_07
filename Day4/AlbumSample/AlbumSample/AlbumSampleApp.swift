060cc7f437a5f86bb62a9e385bafb44fc61d882d
Kiyong Kim <scgyong@gmail.com>
2023-07-13 16:40:05 +0900

    AlbumSample Project skeleton

---
+++ Day4/AlbumSample/AlbumSample/AlbumSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumSampleApp.swift
+//  AlbumSample
+//
+//  Created by Kiyong Kim on 2023/07/13.
+//
+
+import SwiftUI
+
+@main
+struct AlbumSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
